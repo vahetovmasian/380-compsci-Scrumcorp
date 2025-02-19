e8cfdc52445d05a55e6dbf1b2ac402486ab05345
vahetovmasian <vahe.tovmasian.888@my.csun.edu>
2025-02-19 15:04:48 -0800

    Update Product.java

--- Product.java
+++ Product.java
@@ -1,4 +1,22 @@
+  /* Class that repersents a Product it is composed of:
+     year:      a int to represent the year the product came out
+     title:     a string to hold the products title
+     price:     a int that holds the products price to purchase
+     artist:    a string that holds the name(s) of the artist(s) who made the product
+     trackList: an array of strings that has all the songs on the product
+     mediaType: a string that holds the name of what type of media EX(CD,Vinyl,Cassette
+
+     This Class will have methods that include:
+  */ 
 
 public class Product {
+  //Empty constructor     
+    public Product (){
+    
+    }
+  //Constructor with conditions
+    public Product(){
 
+    }
+    
 }
