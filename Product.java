10c16681bd15ff3d740cc457f1af299a4ad710fc
LeoYegh <leo.yeghiayan.246@my.csun.edu>
2025-02-19 15:04:05 -0800

    Update Product.java

--- Product.java
+++ Product.java
@@ -1,4 +1,12 @@
-
 public class Product {
+  String title;
+  String Genre;
+  int price;
+  int releaseYear;
+  String artist;
+  int productNum;
+  String mediaType;
+  String track
 
+  public 
 }
