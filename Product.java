333e5b34768fd282a0ddf1f6881dbd01c2c41a8c
LeoYegh <leo.yeghiayan.246@my.csun.edu>
2025-02-19 15:17:47 -0800

    Update Product.java

--- Product.java
+++ Product.java
@@ -1,12 +1,21 @@
 public class Product {
   String title;
-  String Genre;
-  int price;
+  String genre;
+  double price;
   int releaseYear;
-  String artist;
+  String[] artist;
   int productNum;
   String mediaType;
-  String track
+  String[] track;
 
-  public 
+  public Product(String title, String genre, double price, int releaseYear, String[] artist, int productNum, String mediaType, String[] track) {
+    this.title = title;
+    this.genre = genre;
+    this.price = price;
+    this.releaseYear = ReleaseYear;
+    this.artist = artist;
+    this.productNum = productNum;
+    this.mediaType = mediaType;
+    this.track = track;
+  }
 }
