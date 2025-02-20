5929083766ebef53e4f1e7e5ce329d8028ca06b2
vahetovmasian <vahe.tovmasian.888@my.csun.edu>
2025-02-20 00:31:01 -0800

    Update Page.java
    Just added some commenting and empty constructor for Page

--- Page.java
+++ Page.java
@@ -1,4 +1,17 @@
+/*The Page class is supposed to represent a webpage which is the GUI portion of the 
+website that users interact with. It will have 
+METHODS:
+    Scroll:   a person can scroll up and down webpage
+    GUI:      a method for actually displaying 
+    Navigate: a method from going from one page to another
+RESPONSIBILITYS:
+    Search:   calls upon class to search for things ???
 
+*/
 public class Page {
+    // Empty constructor for Page
+    public Page(){
 
+    }
 }
+
