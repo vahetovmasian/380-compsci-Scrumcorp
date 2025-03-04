f81c142933779312ec9c42b97ef0e727dc0d7539
ethan1019783 <ethan.gonzalez.553@my.csun.edu>
2025-03-04 11:58:21 -0800

    Update and rename ItemPanel.java to productPanel.java

---
+++ productPanel.java
@@ -0,0 +1,89 @@
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.security.DigestException;
+import java.awt.Button;
+import javax.swing.ImageIcon;
+import java.awt.Font;
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.border.Border;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class productPanel extends JPanel implements ActionListener {
+   
+   String albumName;
+   String albumArtist; 
+   String genre; 
+   String coverFileName;
+   
+   JLabel itemLabel = new JLabel();
+   JLabel artistLabel = new JLabel();
+   JPanel spacer = new JPanel(); 
+  
+
+   productPanel(String albumName,String albumArtist, String genre, String coverFileName) {
+      Border border = BorderFactory.createLineBorder(Color.black, 1);
+      this.albumName = albumName;
+      this.albumArtist = albumArtist; 
+      this.genre = genre;
+      this.coverFileName = coverFileName; 
+
+      spacer.setPreferredSize(new Dimension(20,18));
+      spacer.setBackground(Color.WHITE);
+      
+      ImageIcon image = new ImageIcon("products/" + coverFileName);
+      itemLabel.setText(albumName);
+      itemLabel.setFont(new Font("Calibri", Font.BOLD,16));
+      itemLabel.setIcon(image);
+      itemLabel.setVerticalTextPosition(JLabel.BOTTOM);
+      itemLabel.setHorizontalTextPosition(JLabel.CENTER);
+      itemLabel.setHorizontalAlignment(JLabel.CENTER);
+      itemLabel.setPreferredSize(new Dimension(250,250));
+      itemLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+      artistLabel.setText(albumArtist);
+      artistLabel.setFont(new Font("Calibri", Font.PLAIN,12));
+      artistLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+      JButton viewButton = new JButton("VIEW ITEM");
+      viewButton.setPreferredSize(new Dimension(260,50));
+      viewButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+      this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+      this.add(Box.createVerticalGlue());
+      this.setBorder(border);
+      this.setName(albumName);
+      this.setPreferredSize(new Dimension(300,350));
+      this.setBackground(Color.white);
+      this.add(itemLabel);                                                 // has image + albumTitle 
+      this.add(artistLabel);
+      this.add(spacer);
+      this.add(viewButton);
+   }
+
+   public String getAlbumName(){
+      return albumName.toString(); 
+   }
+
+
+   @Override
+   public void actionPerformed(ActionEvent e) {                   // this would be for the view button 
+
+   }
+
+
+}
