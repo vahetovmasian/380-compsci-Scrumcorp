e3a5ea9df94e99bcd503c45446d1439471bb1976
sareher04 <sarah.hermida.306@my.csun.edu>
2025-03-10 17:12:17 -0700

    Update Search.java

--- Search.java
+++ Search.java
@@ -1,4 +1,36 @@
-
 public class Search {
 
-}
+   private Product[] productArray;
+   
+   public Search(Product[] products) { //constructor to initialize search with the array of products 
+      this.productArray = products;
+   }
+   
+   public Product[] search(String query) { //search method to return products matching the query
+      Product[] results = new Product[productArray.length]; //array storing matching products
+      int count = 0; //tracks how many matches 
+      
+      String lowerCaseQuery = query.toLowerCase(); //turns query to lower case for case-insensitive search
+      
+      
+      for (Product product : productArray) { //iterate through array of products 
+            String title = product.getTitle().toLowerCase();
+            String genre = product.getGenre().toLowerCase();
+            String artist = product.getArtist().toLowerCase();
+            
+            if (title.contains(lowerCaseQuery) || genre.contains(lowerCaseQuery) || artist.contains(lowerCaseQuery) { //checks the title, genre, and artist attributes
+               results[count] = product; // assigns the matched product to  the index of the value of count 
+               count++; // increments counter because a product was found 
+            }
+      }
+
+      if (count == 0) { //no results found 
+         return new Product[0]; //empty array 
+      } 
+      
+      Product [] finalResults = new Product[count]; //creates an array with the number of results 
+      System.arraycopy(results, 0, finalResults, 0, count); //copy results into final array
+      return finalResults; //return the array with correct size
+      
+}  
+        
