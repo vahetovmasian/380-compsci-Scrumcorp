668ad49bffea5aabf9b1285c8060fadafde3d6b7
sareher04 <sarah.hermida.306@my.csun.edu>
2025-03-10 23:56:28 -0700

    Update Search.java

--- Search.java
+++ Search.java
@@ -11,27 +11,37 @@ public class Search {
       int count = 0; //tracks how many matches 
       
       String lowerCaseQuery = query.toLowerCase(); //turns query to lower case for case-insensitive search
+      String[] keywords = lowerCaseQuery.split("\\s+"); 
       
-      
-      for (Product product : productArray) { //iterate through array of products 
+      for (Product product : productArray) { //iterate through array of products
+            boolean isMatch = true ; // assumes product is a match
             String title = product.getTitle().toLowerCase(); 
             String genre = product.getGenre().toLowerCase();
             String artist = product.getArtist().toLowerCase();
             String mediaType = product.getMediaType().toLowerCase();
+           
+           for(String keyword : keywords) { //checks each keyword against the product 
+               boolean keywordFound = false; //assumes the current keyword does not match 
+               
+               if (title.contains(keyword) || genre.contains(keyword) || artist.contains(keyword) || mediaType.contains(keyword)) { //checks the title, genre, and artist attributes
+               keywordFound = true ; // current keyword matches an attribute, move on to next keyword 
+               }
             
-            if (title.contains(lowerCaseQuery) || genre.contains(lowerCaseQuery) || artist.contains(lowerCaseQuery) || mediaType.contains(lowerCaseQuery)) { //checks the title, genre, and artist attributes
-               results[count] = product; // assigns the matched product to  the index of the value of count 
-               count++; // increments counter because a product was found 
-            }
-      }
-
-      if (count == 0) { //no results found 
-         return new Product[0]; //empty array 
-      } 
+               if (!keywordFound) { // current keyword was not found
+                  isMatch = false; // the product is not a match 
+                  break; //exit loop and check next product
+               }
+           }
+        
+        if (isMatch) { // all keywords match, therefore result is valid 
+            results[count]= product; //adds product to result array at index count 
+             count++; //increments count by 1 because a product was added to result 
+        } 
+    }
       
-      Product [] finalResults = new Product[count]; //creates an array with the number of results 
-      System.arraycopy(results, 0, finalResults, 0, count); //copy results into final array of size count 
-      return finalResults; //return the array with correct size
+      Product [] finalResults = new Product[count]; //creates an new array with the correct size 
+      System.arraycopy(results, 0, finalResults, 0, count); //copy results into final array 
+      return finalResults; //return final array 
       
    }
 }
