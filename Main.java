9ba55169a73484885dae73d7425a87d05f14c0ef
ethan1019783 <ethan.gonzalez.553@my.csun.edu>
2025-03-24 15:09:00 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,4 +1,9 @@
+import java.io.FileNotFoundException;
 
 public class Main {
+    public static void main(String[] args) throws FileNotFoundException{
+    Page commercePage = new Page();
 
+
+    }   
 }
