1a5ab53e3d2a920deffb49b0386416d86ce72763
ethan1019783 <ethan.gonzalez.553@my.csun.edu>
2025-03-30 00:08:10 -0700

    Update productPanel.java

--- productPanel.java
+++ productPanel.java
@@ -1,3 +1,4 @@
+import javax.imageio.ImageIO;
 import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
@@ -8,10 +9,13 @@ import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.Image;
 import javax.swing.JLabel;
 import javax.swing.border.Border;
 import java.awt.event.ActionListener;
-import java.io.FileNotFoundException;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.awt.event.ActionEvent;
 
 public class productPanel extends JPanel implements ActionListener {
@@ -25,7 +29,7 @@ public class productPanel extends JPanel implements ActionListener {
    JPanel spacer = new JPanel(); 
   
 
-   productPanel(String albumName,String albumArtist, String genre, String coverFileString) throws FileNotFoundException {
+   productPanel(String albumName,String albumArtist, String genre, String coverFileString) throws IOException {
       Border border = BorderFactory.createLineBorder(Color.black, 1);
       this.albumName = albumName;
       this.albumArtist = albumArtist; 
@@ -33,16 +37,29 @@ public class productPanel extends JPanel implements ActionListener {
       this.coverFileString = coverFileString; 
 
       spacer.setPreferredSize(new Dimension(20,18));
-      spacer.setBackground(Color.WHITE);
-      
-      ImageIcon image = new ImageIcon("products/" + coverFileString);
+      spacer.setBackground(Color.WHITE);      
+
+      try {
+         BufferedImage image = ImageIO.read(new File("products/" + coverFileString));
+         Image resizedImage = image.getScaledInstance(200, 200, Image.SCALE_SMOOTH);
+         ImageIcon imageIcon = new ImageIcon(resizedImage);
+         itemLabel.setIcon(imageIcon);
+
+      } catch (Exception e) {
+         BufferedImage image = ImageIO.read(new File("products/" + "disk.png"));
+         Image resizedImage = image.getScaledInstance(200, 200, Image.SCALE_SMOOTH);
+         ImageIcon imageIcon = new ImageIcon(resizedImage);
+         itemLabel.setIcon(imageIcon);
+         System.out.println(coverFileString);
+      }
+
+
       itemLabel.setText(albumName);
       itemLabel.setFont(new Font("Calibri", Font.BOLD,16));
-      itemLabel.setIcon(image);
       itemLabel.setVerticalTextPosition(JLabel.BOTTOM);
       itemLabel.setHorizontalTextPosition(JLabel.CENTER);
       itemLabel.setHorizontalAlignment(JLabel.CENTER);
-      itemLabel.setPreferredSize(new Dimension(250,250));
+      itemLabel.setPreferredSize(new Dimension(295,250));
       itemLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
 
       artistLabel.setText(albumArtist);
@@ -57,7 +74,7 @@ public class productPanel extends JPanel implements ActionListener {
       this.add(Box.createVerticalGlue());
       this.setBorder(border);
       this.setName(albumName);
-      this.setPreferredSize(new Dimension(300,350));
+      this.setSize(new Dimension(400,350));
       this.setBackground(Color.white);
       this.add(itemLabel);                                                 // has image + albumTitle 
       this.add(artistLabel);
@@ -71,7 +88,7 @@ public class productPanel extends JPanel implements ActionListener {
 
 
    @Override
-   public void actionPerformed(ActionEvent e) {                   // this would be for the view button 
+   public void actionPerformed(ActionEvent e) {                   // button in a productPanel to open product page  
 
    }
 
