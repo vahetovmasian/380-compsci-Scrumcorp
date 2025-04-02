037c8f5aec713fd4cc5faaa4deb618a921d6e369
ethan1019783 <ethan.gonzalez.553@my.csun.edu>
2025-04-02 10:03:24 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,6 +3,6 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException{
         WindowManager myWindow = new WindowManager(); 
-        
+        //////
     }   
 }
