72e7f4d4fec1d6ca2d4b030f520a840d5bfde4ac
ethan1019783 <ethan.gonzalez.553@my.csun.edu>
2025-04-16 15:14:32 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,14 @@
 import java.io.IOException;
 
+import javax.swing.UIManager;
+
 public class Main {
     public static void main(String[] args) throws IOException{
         WindowManager myWindow = new WindowManager(); 
-        //////
+               try {
+    UIManager.setLookAndFeel( UIManager.getCrossPlatformLookAndFeelClassName() );
+ } catch (Exception e) {
+            e.printStackTrace();
+ }
     }   
 }
