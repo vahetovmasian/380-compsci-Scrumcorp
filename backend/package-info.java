ef22419acb9a3ae8f028710286848a626ad94413
vahetovmasian <vahe.tovmasian.888@my.csun.edu>
2025-05-05 17:57:02 -0700

    Delete backend directory

--- backend/package-info.java
+++
@@ -1,5 +0,0 @@
-package backend;
-/**This package handles the backend
- * the backend includes searching, accessing and modifying the database 
- * It also handles passing this information to other classes and organizing it.
- */
